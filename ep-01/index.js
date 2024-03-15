97db7f8f1aaec8076f34118141722b60d4de5806
Debajyoti Hui <debajyotihui1707@gmail.com>
2024-03-15 10:41:32 +0530

    ep-01

---
+++ ep-01/index.js
@@ -0,0 +1,16 @@
+//Template literals:
+const age = 21;
+const fullName = "Debajyoti Hui";
+
+//Output: Hello Mr Debajyoti Hui your age is: 20
+
+console.log("Hello Mr "+fullName+" your age is: "+age);
+
+console.log(`Hello Mr ${fullName} your age is: ${age}`);
+
+//Q. Print sum of two numbers
+const a = 10;
+const b = 20;
+
+console.log("Sum of "+a+" and "+b+" is: "+a+b);
+console.log(`Sum of ${a} and ${b} is ${a+b}`);
